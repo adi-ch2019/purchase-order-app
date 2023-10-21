89dde7538579b70a19c9ebf9ec4bc166ca52d95b
adi-ch2019 <adi.ch2019@outlook.com>
2023-10-21 16:16:14 +0530

    mergeMap and switchMap

--- src/Frontend/adi.ch.dev.poa.ui/src/services/DataService.ts
+++ src/Frontend/adi.ch.dev.poa.ui/src/services/DataService.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { concatMap, map, of, tap } from 'rxjs';
+import { concatMap, map, mergeMap, of, switchMap, tap } from 'rxjs';
 import { IOrder } from 'src/Interfaces/Order';
 import { environment } from 'src/environment';
 @Injectable({
@@ -18,11 +18,34 @@ ordersWithConcatMap$=of(1,4)
   concatMap(id => this.http.get<IOrder[]>(`${environment.favoriteUrl}/Order/GetWithId/?id=${id}`)
 ));
 
+ordersWithMergeMap$=of(1,4)
+.pipe(
+  tap(id=>console.log('mergeMap source Observable',id)),
+  mergeMap(id => this.http.get<IOrder[]>(`${environment.favoriteUrl}/Order/GetWithId/?id=${id}`)
+));
+
+ordersWithSwitchMap$=of(1,4)
+.pipe(
+  tap(id=>console.log('switchMap source Observable',id)),
+  switchMap(id => this.http.get<IOrder[]>(`${environment.favoriteUrl}/Order/GetWithId/?id=${id}`)
+));
+
+
+
   constructor(private http: HttpClient) {
 
     this.ordersWithConcatMap$.subscribe(
       item=>console.log('Concatmap result',item)
     );
+
+    this.ordersWithMergeMap$.subscribe(
+      item=>console.log('Mergemap result',item)
+    );
+
+    this.ordersWithSwitchMap$.subscribe(
+      item=>console.log('Switchmap result',item)
+    );
+    
     // this.ordersWithMap$.subscribe(
     //   item=>console.log('map result',item)
     // );
