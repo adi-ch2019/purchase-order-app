5412068e07b8248512b884e83da68a74ee52fb3d
adi-ch2019 <adi.ch2019@outlook.com>
2023-11-06 15:15:53 +0530

    login view models

---
+++ src/Backend/adi.ch.dev.poa.api/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adi.ch.dev.poa.api.Data
+{
+    public class LoginVM
+    {
+        [Required]
+        public required string EmailAddress { get; set; }
+        
+        [Required]
+        public required string Password { get; set; }
+    }
+}
---
+++ src/Backend/adi.ch.dev.poa.api/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adi.ch.dev.poa.api.Data
+{
+    public class RegisterVM
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        [Required]
+        public required string EmailAddress { get; set; }
+        [Required]
+        public required string UserName { get; set; }
+        [Required]
+        public required string Password { get; set; }
+    }
+}
